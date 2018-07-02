c5dcee5f3445e59f6a1ab0f5cc8a5542761df494
Kzulfazriawan <kzul@localhost.localdomain>
2018-07-02 21:00:11 +0700

    new simplify function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
 
 
 class CalculatorConverter(GridLayout):
@@ -13,21 +14,25 @@ class CalculatorConverter(GridLayout):
     input_value    = NumericProperty
     input_type     = NumericProperty
 
-    binnary_result = NumericProperty
-    octal_result   = NumericProperty
-    decimal_result = NumericProperty
-    hexa_result    = NumericProperty
+    binary_result  = StringProperty
+    octal_result   = StringProperty
+    decimal_result = StringProperty
+    hexa_result    = StringProperty
 
-    def converting_from(self, *args):
-        if self.input_type == 1:
-            self.binnary_result = int(self.input_value, 2)
-
-        elif self.input_type == 7:
-            pass
-        elif self.input_type == 16:
+    def converting(self, value):
+        try:
+            v = int(value)
+        except ValueError:
             pass
         else:
-            pass
+            if self.input_type in [2, 8, 16]:
+                d = int(str(v), self.input_type)
+                self.decimal_result = str(d)
+                self.binary_result = '{:0b}'.format(d)
+                self.octal_result = '{:0o}'.format(d)
+                self.hexa_result = '{:0x}'.format(d)
+
+                print(self.binary_result, self.decimal_result, self.octal_result, self.hexa_result)
 
 
 class ConverterApp(App):
